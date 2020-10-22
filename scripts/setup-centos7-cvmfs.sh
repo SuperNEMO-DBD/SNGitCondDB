6e092c8dabf8a4b497928daef4408ef93a397001
Ben Morgan <ben.morgan@warwick.ac.uk>
2020-10-22 17:38:15 +0100

    Add setup script for LCG/CentOS7

---
+++ scripts/setup-centos7-cvmfs.sh
@@ -0,0 +1,3 @@
+source /cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc9-opt/setup.sh
+# Additional PKG_CONFIG_PATH needed for openssl
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib64/pkgconfig
